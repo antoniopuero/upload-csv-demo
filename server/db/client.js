b439b3c947e90a6a8d6c4bedc00b8bc644f00bae
Anton Savchenko <anton.savchenko@datarobot.com>
2018-05-27 18:47:26 +0300

    feat(sql escape): added sql strings escape

--- server/db/client.js
+++ server/db/client.js
@@ -1,5 +1,7 @@
 import sqlite3 from 'sqlite3';
 import config from '../../config';
+import map from 'lodash/map';
+import sqlString from 'sqlstring';
 
 if (config.get('env') === 'dev') {
   sqlite3.verbose();
@@ -47,17 +49,18 @@ export function insertStatement() {
 }
 
 export function insertValues(stm, values) {
-  return promisify(stm, values);
+  return promisify(stm, map(values, value => sqlString.escape(value)));
 }
 
 export function finalizeStatement(stm) {
   return stm.finalize();
 }
 
-export function searchValuesByName(name = '', limit = 20, offset = 0) {
+export function searchValuesByName(query = '', limit = 20, offset = 0) {
+  const escapedQuery = sqlString.escape(query);
   return new Promise((resolve, reject) => {
     db.all(
-      `SELECT * FROM people WHERE name LIKE '%${name}%' LIMIT ${limit} OFFSET ${offset}`,
+      `SELECT * FROM people WHERE name LIKE '%${escapedQuery}%' LIMIT ${limit} OFFSET ${offset}`,
       (err, result) => {
         if (err) {
           return reject(err);
